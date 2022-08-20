ca3874f27c8244f4b16ba33f83279790f20e09d9
Pro-Developer01 <yadavshashank70@gmail.com>
2022-08-21 01:14:19 +0530

    Navbar Complet

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,42 +1,27 @@
 import React from 'react'
+import "./NavbarStyle.css"
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">E-Shop</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href='/'>Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+    // <nav className="navbar navbar-expand-lg bg-light">
+      <div className="containerNav">
+
+      <div className="logoNav">
+        <img src="https://www.logolynx.com/images/logolynx/90/90cdeb260df361a39d97540d9c75a814.png" height={"36px"} alt="" />
+        <span style={{fontSize:"22px"}}>E-Shop</span>
+      </div>
+      <div className="MiddleNav">
+        <span>Men</span>
+        <span>Women</span>
+        <span>Child</span>
+      </div>
+      <div className="lastNav">
+        <img src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/search-512.png" height={"24px"} alt="" />
+        <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" height={"24px"} alt="" />
+        <img src="https://static.thenounproject.com/png/4693049-200.png" height={"30px"} alt="" />
+       
+      </div>
+      </div>
+// </nav>
   )
 }
